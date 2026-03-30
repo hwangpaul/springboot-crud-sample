555030cea8d98f3b06706d24a7dfe84330e19acf
hwangpaul <71800750+hwangpaul@users.noreply.github.com>
2026-03-30 14:45:50 +0900

    Create CrudSampleApplication.java

---
+++ com/sample/crud/CrudSampleApplication.java
@@ -0,0 +1,12 @@
+package com.sample.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSampleApplication.class, args);
+    }
+}
