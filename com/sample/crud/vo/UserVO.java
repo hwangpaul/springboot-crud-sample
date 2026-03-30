c241e97ed8db2ab5ad92c396293dee3e5e57ea18
hwangpaul <71800750+hwangpaul@users.noreply.github.com>
2026-03-30 14:44:47 +0900

    Create UserVO.java

---
+++ com/sample/crud/vo/UserVO.java
@@ -0,0 +1,23 @@
+package com.sample.crud.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserVO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
